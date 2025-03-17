0059bf387d4a632411cc335ca6b63abca82fa5dc
isandy <zhangch_@hotmail.com>
2025-03-17 16:57:31 +0800

    初始版本，集成基本组件!

---
+++ src/main/java/com/jq/jq_server/JqServerApplication.java
@@ -0,0 +1,13 @@
+package com.jq.jq_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JqServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JqServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jq/jq_server/JqServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jq.jq_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JqServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
